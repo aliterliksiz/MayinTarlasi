96033aae61f2515fbaef0b72fd55bc820fa5b672
aliterliksiz <aliterliksiz46@gmail.com>
2022-05-17 01:19:00 +0300

    Basic Minesweeper Game with JAVA

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int row,column;
+        System.out.println("Mayın Tarlasına Hoşgeldiniz");
+        System.out.println("Lütfen oynamak istediğiniz boyutları giriniz");
+        //System.out.print("Satır Sayısı");
+        //row=scan.nextInt();
+        //System.out.print("Sütün Sayısı");
+        //column=scan.nextInt();
+        MayinTarlasi mayin= new MayinTarlasi(2,2);
+        mayin.run();
+    }
+}
